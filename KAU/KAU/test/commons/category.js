28c9fce3ec92403c2ff9b3083a1e8e872c61e446
tesorieror <tesorieror@gmail.com>
2015-04-13 23:59:07 +0200

    Category

---
+++ KAU/KAU/test/commons/category.js
@@ -0,0 +1,13 @@
+function Category() {
+	this.name = null;
+	this.subCategories = [];
+}
+
+Category.prototype.fromJSON = function(data) {
+	this.name
+}
+
+app.controller('TempCtrl', function($scope, $log, $location, dataStoreService) {
+	$log.info("Temp controller running");
+
+});
